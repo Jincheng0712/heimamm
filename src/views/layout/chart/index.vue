8a0c942652cb23fbcd429595e6b099fa09f8718f
Jincheng0712 <1373743789@qq.com>
2020-06-11 16:28:48 +0800

    第12天-完成后台数据预览

--- src/views/layout/chart/index.vue
+++ src/views/layout/chart/index.vue
@@ -1,3 +1,163 @@
 <template>
-  <div>我是数据预览</div>
+  <div class="chart">
+    <!-- 上部分 -->
+    <el-card>
+      <ul class="list">
+        <li>
+          <div class="itemContent color1">{{titleObj.increment_users}}</div>
+          <p>今日用户新增</p>
+        </li>
+
+        <li>
+          <div class="itemContent color1">{{titleObj.total_users}}</div>
+          <p>总用户量</p>
+        </li>
+
+        <li>
+          <div class="itemContent color2">{{titleObj.increment_questions}}</div>
+          <p>新增试题</p>
+        </li>
+
+        <li>
+          <div class="itemContent color2">{{titleObj.total_questions}}</div>
+          <p>总试题量</p>
+        </li>
+
+        <li>
+          <div class="itemContent color3">{{titleObj.personal_questions}}</div>
+          <p>人均刷题量</p>
+        </li>
+
+        <li>
+          <div class="itemContent color3">{{titleObj.total_done_questions}}</div>
+          <p>总刷题量</p>
+        </li>
+      </ul>
+    </el-card>
+    <!-- 下部分 -->
+    <el-card style="margin-top:15px;">
+      <!-- echarts图标的容器 -->
+      <div class="echarts" ref="echartsRef"></div>
+    </el-card>
+  </div>
 </template>
+
+<script>
+import echarts from "echarts";
+export default {
+  name: "Chart",
+  data() {
+    return {
+      titleObj: {}, // 统计人数、刷题量等
+      statistics: [] // 统计哪些企业
+    };
+  },
+  created() {
+    this.getTitleData();
+  },
+  mounted() {
+    this.getStatisticsData();
+  },
+  methods: {
+    async getTitleData() {
+      const res = await this.$axios.post("/data/title");
+      if (res.data.code === 200) {
+        // console.log(res.data);
+        this.titleObj = res.data.data;
+      }
+    },
+    async getStatisticsData() {
+      const res = await this.$axios.post("/data/statistics");
+      if (res.data.code === 200) {
+        this.statistics = res.data.data;
+        this.initEcharts();
+      }
+    },
+    // 初始化echars
+    initEcharts() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(this.$refs.echartsRef);
+      // 把statistics中的name取出来生成一个新的数组 给图例的data使用
+      const legendData = this.statistics.map(item => item.name);
+
+      // 指定图表的配置项和数据
+      var option = {
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          right: 10,
+          data: legendData
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["50%", "70%"],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: "center"
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: "30",
+                fontWeight: "bold"
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: this.statistics
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+  }
+};
+</script>
+
+
+<style lang="less">
+.chart {
+  .list {
+    //弹性例子布局
+    display: flex;
+    //主轴分布
+    justify-content: space-around;
+    //侧轴分布
+    align-items: center;
+    //文本居中
+    text-align: center;
+    //颜色处理
+    .color1 {
+      border: 3px solid #0086fa;
+      color: #0086fa;
+    }
+    .color2 {
+      border: 3px solid #f76137;
+      color: #f76137;
+    }
+    .color3 {
+      border: 3px solid #55cd78;
+      color: #55cd78;
+    }
+    //画圈圈
+    .itemContent {
+      border-radius: 50%;
+      width: 99px;
+      height: 99px;
+      line-height: 99px;
+    }
+  }
+  .echarts {
+    height: 500px;
+  }
+}
+</style>
